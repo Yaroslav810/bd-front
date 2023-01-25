841f1da8215df0f386bc1e297391fa5cc2f07245
Tatiana Fadeeva <tatianafadeevaalek@gmail.com>
2023-01-25 21:56:16 +0300

    поменяла страницу фаворитов, нужно разобраться с бэком

--- src/pages/favorites/Favorites.tsx
+++ src/pages/favorites/Favorites.tsx
@@ -1,4 +1,4 @@
-function Favorites() {
+/*function Favorites() {
   return <div>
     <a href="/">Главная</a>
   </div>
@@ -6,4 +6,35 @@ function Favorites() {
 
 export {
   Favorites
+}
+*/
+
+import { ErrorContent } from '../../common/error/ErrorContent'
+import { List } from '../main/components/List'
+import { favoriteStates } from './model/states'
+import { Preloader } from '../../common/preloader/Preloader'
+import styles from '../main/Main.module.css'
+import { useAtom } from '@reatom/npm-react'
+
+
+function Favorites() {
+    const [data] = useAtom(favoriteStates.events)
+    const [isLoading] = useAtom(favoriteStates.loaded)
+
+    let content
+    if (isLoading > 0) {
+        content = <Preloader />
+    } else {
+        content = (data != null)
+            ? <List events={data} />
+            : <ErrorContent />
+    }
+
+    return <div className={styles.main}>
+        {content}
+    </div>
+}
+
+export {
+    Favorites
 }
