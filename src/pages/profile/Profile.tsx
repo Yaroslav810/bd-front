20e0f7f29233dfcf81ab074bb146f61b996e6447
Yaroslav Kuzmin <Yaroslav.Kuzmin.01@yandex.ru>
2023-01-26 17:27:23 +0300

    Что-то сделал

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -4,9 +4,8 @@ import { profileStates } from './model/states'
 import { Preloader } from '../../common/preloader/Preloader'
 import styles from './Profile.module.css'
 import { useAtom } from '@reatom/npm-react'
-import { Typography } from '@mui/material'
-import { getMockCurrentUser } from '../../api/user/mockData'
-import { Avatar } from '@mui/material'
+import { Typography, Avatar } from '@mui/material'
+import { getUser } from '../../model/states'
 
 function Profile() {
   const [data] = useAtom(profileStates.events)
@@ -21,28 +20,24 @@ function Profile() {
       ? <List events={data} />
       : <ErrorContent />
   }
-    let infoUser
-    if (user)
-    {
-        infoUser = <div>
-            <span className={styles.firstName}>{user.firstName}</span>
-            <span> </span>
-            <span className={styles.lastName}>{user.lastName}</span>
-            <p className={styles.login}>{user.login}</p>
-        </div>
-    }
+  let infoUser
+  if (user) {
+    infoUser = <div className={styles.user}>
+      <span className={styles.firstName}>Имя: {user.firstName}</span>
+      <span className={styles.lastName}>{user.lastName}</span>
+      <p className={styles.login}>Логин: {user.login}</p>
+    </div>
+  }
 
   return <div className={styles.profile}>
-      <div className={styles.infoProfile}>
-          <Typography variant="h4" gutterBottom>
-              Профиль
-          </Typography>
-          
-              <Avatar src="/static/images/avatar/2.jpg" className={styles.avatar}/>
-          
-          {infoUser}
-      </div>
-      <div className={styles.content}> 
+    <div className={styles.infoProfile}>
+      <Typography variant="h4" gutterBottom>
+        Профиль
+      </Typography>
+      <Avatar src="/static/images/avatar/2.jpg" className={styles.avatar}/>
+      {infoUser}
+    </div>
+    <div className={styles.content}>
       {content}
     </div>
   </div>
